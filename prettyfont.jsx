b09f297c6555a0a90b806580ce057b0cd5bf8ccf
Sergej Pehterev <colorage503@gmail.com>
2015-02-25 12:48:52 +0300

    Create prettyfont.jsx
    Added Pretty My Font script

---
+++ prettyfont.jsx
@@ -0,0 +1,73 @@
+var selectedLayers = getSelectedLayersIdx(),
+       i;
+ 
+for(i = 0; i < selectedLayers.length; i++){
+  makeActiveByIndex([selectedLayers[i]], false);
+  
+  if (app.activeDocument.activeLayer.kind == LayerKind.TEXT){
+    app.activeDocument.activeLayer.textItem.size = Math.floor (app.activeDocument.activeLayer.textItem.size);
+    if((app.activeDocument.activeLayer.textItem.size.value % 2) !=0){
+        app.activeDocument.activeLayer.textItem.size++;
+    }
+    app.activeDocument.activeLayer.textItem.leading = Math.floor (app.activeDocument.activeLayer.textItem.leading);
+    if((app.activeDocument.activeLayer.textItem.leading.value % 2) !=0){
+        app.activeDocument.activeLayer.textItem.leading++;
+    }
+   }
+}
+ 
+// ================================================
+// FUNCTIONS
+// ================================================
+function getSelectedLayersIdx(){
+  var selectedLayers = new Array;
+  var ref = new ActionReference();
+  ref.putEnumerated( charIDToTypeID("Dcmn"),
+  charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );
+  var desc = executeActionGet(ref);
+  if( desc.hasKey( stringIDToTypeID( 'targetLayers' ) ) ){
+    desc = desc.getList( stringIDToTypeID( 'targetLayers' ));
+    var c = desc.count
+    var selectedLayers = new Array();
+    for(var i=0;i<c;i++){
+      try{
+        activeDocument.backgroundLayer;
+        selectedLayers.push( desc.getReference( i ).getIndex() );
+      }catch(e){
+        selectedLayers.push( desc.getReference( i ).getIndex()+1 );
+      }
+    }
+  }else{
+    var ref = new ActionReference();
+    ref.putProperty( charIDToTypeID("Prpr") , charIDToTypeID( "ItmI" ));
+    ref.putEnumerated( charIDToTypeID("Lyr "),
+    charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );
+    try{
+      activeDocument.backgroundLayer;
+      selectedLayers.push(
+      executeActionGet(ref).getInteger(charIDToTypeID( "ItmI" ))-1);
+    }catch(e){
+      selectedLayers.push(
+      executeActionGet(ref).getInteger(charIDToTypeID( "ItmI" )));
+    }
+  }
+  return selectedLayers;
+}
+ 
+function makeActiveByIndex(idx, visible) {
+  
+    for (var i = 0; i < idx.length; i++) {
+        var desc = new ActionDescriptor();
+        var ref = new ActionReference();
+        ref.putIndex(charIDToTypeID("Lyr "), idx[i])
+        desc.putReference(charIDToTypeID("null"), ref);
+        if (i > 0) {
+            var idselectionModifier = stringIDToTypeID("selectionModifier");
+            var idselectionModifierType = stringIDToTypeID("selectionModifierType");
+            var idaddToSelection = stringIDToTypeID("addToSelection");
+            desc.putEnumerated(idselectionModifier, idselectionModifierType, idaddToSelection);
+        }
+        desc.putBoolean(charIDToTypeID("MkVs"), visible);
+        executeAction(charIDToTypeID("slct"), desc, DialogModes.NO);
+    }
+}
